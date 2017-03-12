e3ee7869a473ed705fc4e45d902773e80373333b
DmitryKozhurkin <kozhurkin.dima@gmail.com>
2017-03-12 13:49:41 +0300

    DayPicker

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import React, { Component, PropTypes } from 'react'
 import ArticleList from './ArticleList/index'
 import Chart from './Chart'
 import Select from 'react-select'
+import DayPicker, { DateUtils } from 'react-day-picker'
 import 'react-select/dist/react-select.css'
+import 'react-day-picker/lib/style.css'
 
 class App extends Component {
     static propTypes = {
@@ -11,10 +13,13 @@ class App extends Component {
 
     state = {
         text: '',
-        selected: null
+        selectedArticle: null,
+        dayFrom: null,
+        dayTo: null,
     }
 
     render() {
+        const { selectedArticle, dayFrom, dayTo } = this.state
         const { articles } = this.props
         const options = articles.map(article => ({
             label: article.title,
@@ -23,7 +28,10 @@ class App extends Component {
         return (
             <div>
                 Enter your name: <input type="text" value={this.state.text} onChange={this.handleTextChange}/>
-                <Select options = {options} value={this.state.selected} onChange = {this.handleSelectChange} multi/>
+                <Select options = {options} value={selectedArticle} onChange = {this.handleSelectChange} multi/>
+                <p><a href="#" onClick={this.handleResetClick}>Reset</a></p>
+                <p>{ dayFrom && dayTo && <span>You chose from { '' + dayFrom } to { '' + dayTo } </span> }</p>
+                <DayPicker numberOfMonths={ 2 } selectedDays={ [dayFrom, { from: dayFrom, to: dayTo }] } onDayClick={ this.handleDayClick }/>
                 <ArticleList articles={this.props.articles}/>
                 <Chart articles={this.props.articles}/>
             </div>
@@ -32,6 +40,26 @@ class App extends Component {
 
     handleSelectChange = selected => {
         this.setState({ selected })
+    }
+
+    handleResetClick = (e) => {
+        e.preventDefault();
+        this.setState({
+            ...this.state,
+            dayFrom: null,
+            dayTo: null,
+        });
+    }
+
+    handleDayClick = (day) => {
+        const { dayFrom, dayTo } = this.state
+        const range = DateUtils.addDayToRange(day, { from: dayFrom, to: dayTo });
+        console.log(range);
+        this.setState({
+            ...this.state,
+            dayFrom: range.from || null,
+            dayTo: range.to || null
+        })
     }
 
     handleTextChange = ev => {
